f506dd44e5257eaff27aa0cccc9523d358837479
aymane2799 <aymane.ouladbenhamou@gmail.com>
2023-05-27 18:36:28 +0100

    Authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import { useAuthStore } from "../stores/auth";
+import Home from "./../views/Home.vue";
+import Login from "./../views/Login.vue";
+import Register from "./../views/Register.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: Home,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/login",
+      name: "login",
+      component: Login,
+    },
+    {
+      path: "/register",
+      name: "register",
+      component: Register,
+    },
+  ],
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const authStore = useAuthStore();
+  
+  if (authStore.isAuthenticated && to.name === 'login') next('/');
+  else if (authStore.isAuthenticated && to.name === 'register') next('/');
+  else if (!authStore.isAuthenticated && to.name !== "login" && to.name !== "home" && to.name !== "register")
+    next({ name: "login" });
+  else next();
+});
+
+export default router;
